f697c6bedadacdf951d041273d95d2f960696250
Yusuke Izawa <me@yizawa.com>
2024-07-16 16:37:53 +0900

    Update frontend.py

--- minipypy/frontend.py
+++ minipypy/frontend.py
@@ -4,36 +4,49 @@ import struct
 import sys
 import time
 
+from minipypy.tools.log import debug_print
 
-def load_pyc(fname):
+
+def load_pyc_py3(fname):
     f = open(fname, "rb")
     magic = f.read(4)
-    # print("magic %s" % (binascii.hexlify(magic)))
+    magic = binascii.hexlify(magic)
     read_date_and_size = True
     flags = struct.unpack("<L", f.read(4))[0]
     hash_based = bool(flags & 0x01)
     check_source = bool(flags & 0x02)
-    # print(f"flags {flags:#08x}")
+    debug_print("flags %s" % (flags))
     if hash_based:
         source_hash = f.read(8)
         read_date_and_size = False
-        # print(f"hash {binascii.hexlify(source_hash)}")
-        # print(f"check_source {check_source}")
+        hash = binascii.hexlify(source_hash)
+        debug_print("hash " + str(hash))
+        debug_print("check_source " + str(check_source))
     if read_date_and_size:
         moddate = f.read(4)
         modtime = time.asctime(time.localtime(struct.unpack("<L", moddate)[0]))
-        # print(f"moddate {binascii.hexlify(moddate)} ({modtime})")
+        modddate = binascii.hexlify(moddate)
+        debug_print("moddate %s (%s)" % (moddate, modtime))
         size = f.read(4)
-        # print("pysize %s (%d)" % (binascii.hexlify(size), struct.unpack("<L", size)[0]))
+        debug_print(
+            "pysize %s (%d)" % (binascii.hexlify(size), struct.unpack("<L", size)[0])
+        )
+    code = marshal.load(f)
+    f.close()
+    return code
+
+
+def load_pyc_py2(fname):
+    f = open(fname, "rb")
+    magic = f.read(4)
+    timestamp = f.read(4)
     code = marshal.load(f)
     f.close()
     return code
 
 
 if __name__ == "__main__":
-    import opcode
+    import dis
 
-    code = load_pyc(sys.argv[1])
-    import pdb
-
-    breakpoint()
+    code = load_pyc_py2(sys.argv[1])
+    dis.disassemble(code)
