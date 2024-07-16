63001022020b6e7f7b071672c43faf18e12cb108
Yusuke Izawa <me@yizawa.com>
2024-07-16 16:38:09 +0900

    Implement skeleton of interpreter

--- minipypy/interpret.py
+++ minipypy/interpret.py
@@ -1,4 +1,5 @@
 import frontend
+from opcode import Bytecodes, opmap
 
 def interpret(code):
     bytecode = code.co_code
@@ -7,12 +8,25 @@ def interpret(code):
     stack_ptr = -1
     pc = 0
     while pc < len(bytecode):
-        opcode = bytecode[pc]
-        print(opcode)
+        opcode = ord(bytecode[pc])
+        if opcode == Bytecodes.LOAD_CONST:
+            pass
+        elif opcode == Bytecodes.BINARY_ADD:
+            pass
+        elif opcode == Bytecodes.LOAD_NAME:
+            pass
+        elif opcode == Bytecodes.STORE_NAME:
+            pass
+        elif opcode == Bytecodes.RETURN_VALUE:
+            pass
+        elif opcode == Bytecodes.YIELD_FROM:
+            pass
+        elif opcode == Bytecodes.LOAD_BUILD_CLASS:
+            pass
         pc += 1
 
 
 if __name__ == '__main__':
     import sys
-    code = frontend.load_pyc(sys.argv[1])
+    code = frontend.load_pyc_py2(sys.argv[1])
     interpret(code)
