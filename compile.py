f37112b9f357b7a881f60cf5bfe33036455aa8dc
Yusuke Izawa <me@yizawa.com>
2024-07-22 16:51:09 +0900

    Use py2 compile method

--- compile.py
+++ compile.py
@@ -1,15 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import argparse
+import dis
+import marshal
 import py_compile
 
 def compile_file(path):
     "generate .pyc file"
     py_compile.compile(path)
 
+def compile_py2(path):
+    f = open(args.filename, 'r')
+    code = compile(f.read(), '', 'exec')
+    f.close()
+
+    nameout = args.filename.strip('.py') + '.pyc'
+    fout = open(nameout, 'wb')
+    fout.write(b'0000') # dummy value for magic number
+    fout.write(b'0000') # dummy value for timestamp
+    marshal.dump(code, fout)
+
+    fout.close()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='compile')
     parser.add_argument('filename')
-
     args = parser.parse_args()
-    compile_file(args.filename)
+    compile_py2(args.filename)
