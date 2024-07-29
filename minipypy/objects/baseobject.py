ad9e62229f94b37fb8907dbc8d746737bdc9fee1
Yusuke Izawa <me@yizawa.com>
2024-07-29 19:06:17 +0900

    Fix baseobject.py

--- minipypy/objects/baseobject.py
+++ minipypy/objects/baseobject.py
@@ -1,6 +1,8 @@
+from rpython.rlib.rbigint import rbigint
+
+
 class W_Root(object):
-    def __init__(self, value):
-        self.value = value
+    pass
 
 
 class W_Object(W_Root):
@@ -22,22 +24,31 @@ class W_None(W_Root):
 class W_Int(W_Root):
     typ = "int"
 
+    def __init__(self, value):
+        self.value = value
+
     def __repr__(self):
         return "%d" % (self.value)
 
+    def getvalue(self):
+        return self.value
+
+    def getstr(self):
+        return str(self.value)
+
     def add(self, other):
         if isinstance(other, W_Int):
-            return W_Int(self.value + other.value)
+            return W_Int(self.value + other.getvalue())
         elif isinstance(other, W_Long):
-            return W_Long(self.value + other.value)
+            return W_Long(rbigint.fromint(self.value).add(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
     def sub(self, other):
         if isinstance(other, W_Int):
-            return W_Int(self.value - other.value)
+            return W_Int(self.value - other.getvalue())
         elif isinstance(other, W_Long):
-            return W_Long(self.value - other.value)
+            return W_Long(rbigint.fromint(self.value).sub(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
@@ -45,15 +56,15 @@ class W_Int(W_Root):
         if isinstance(other, W_Int):
             return W_Int(self.value * other.value)
         elif isinstance(other, W_Long):
-            return W_Long(self.value * other.value)
+            return W_Long(rbigint.fromint(self.value).mul(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
     def div(self, other):
         if isinstance(other, W_Int):
-            return W_Int(self.value / other.value)
+            return W_Int(self.value / other.getvalue())
         elif isinstance(other, W_Long):
-            return W_Long(self.value / other.value)
+            return W_Long(rbigint.fromint(self.value).div(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
@@ -61,30 +72,55 @@ class W_Int(W_Root):
 class W_Long(W_Root):
     typ = "long"
 
+    def __init__(self, value):
+        self.value = value  # instance of rbigint
+
+    @staticmethod
+    def fromint(intval):
+        return W_Long(intval)
+
+    @staticmethod
+    def from_rbigint(rbint):
+        return W_Long(rbint)
+
+    def getvalue(self):
+        return self.value
+
+    def getstr(self):
+        return str(self.value)
+
     def __repr__(self):
         return "%dL" % (self.value)
 
     def add(self, other):
-        if isinstance(other, W_Int) or isinstance(other, W_Long):
-            return W_Int(self.value + other.value)
+        if isinstance(other, W_Int):
+            return W_Long(self.value.add(rbigint.fromint(other.getvalue())))
+        elif isinstance(other, W_Long):
+            return W_Long(self.value.add(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
     def sub(self, other):
-        if isinstance(other, W_Int) or isinstance(other, W_Long):
-            return W_Int(self.value - other.value)
+        if isinstance(other, W_Int):
+            return W_Long(self.value.sub(rbigint.fromint(other.getvalue())))
+        elif isinstance(other, W_Long):
+            return W_Long(self.value.sub(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
     def mul(self, other):
-        if isinstance(other, W_Int) or isinstance(other, W_Long):
-            return W_Int(self.value * other.value)
+        if isinstance(other, W_Int):
+            return W_Long(self.value.mul(rbigint.fromint(other.getvalue())))
+        elif isinstance(other, W_Long):
+            return W_Long(self.value.mul(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
     def div(self, other):
-        if isinstance(other, W_Int) or isinstance(other, W_Long):
-            return W_Int(self.value / other.value)
+        if isinstance(other, W_Int):
+            return W_Long(self.value.div(rbigint.fromint(other.getvalue())))
+        elif isinstance(other, W_Long):
+            return W_Long(self.value.div(other.getvalue()))
         else:
             raise Exception("Unexpected object: %s" % (other))
 
@@ -94,16 +130,29 @@ class W_Str(W_Root):
     typ = "str"
 
     def __init__(self, value):
-        self.value = str(value)
+        self.value = value
 
     def __repr__(self):
         return "'%s'" % self.value
 
+    def getvalue(self):
+        return self.value
+
+    def getstr(self):
+        return str(self.value)
+
+    @staticmethod
+    def from_str(strval):
+        assert isinstance(strval, str)
+        return W_Str(strval)
+
     def add(self, other):
-        if isinstance(other, W_Int) or isinstance(other, W_Long):
-            return W_Str(self.value + str(other.value))
-        elif isinstance(other, W_Str) or isinstance(other, W_Byte):
-            return W_Str(self.value + str(other.value))
+        if (
+            isinstance(other, W_Str)
+            or isinstance(other, W_Int)
+            or isinstance(other, W_Long)
+        ):
+            return W_Str(self.getstr() + other.getstr())
         else:
             raise Exception("Unexpected object: %s" % (other))
 
@@ -118,24 +167,32 @@ class W_Str(W_Root):
         else:
             raise Exception("Unexpected object: %s" % (other))
 
+    def div(self, other):
+        raise Exception("Unsupported operaiton /")
+
 
 class W_Byte(W_Root):
 
     typ = "byte"
 
     def __init__(self, value):
-        self.value = bytes(value)
+        self.value = value
 
     def __repr__(self):
         return "b'%b'" % self.value
 
+    def getvalue(self):
+        return self.value
+
+    @staticmethod
+    def from_str(strval):
+        assert isinstance(strval, str)
+        return W_Byte(strval)
+
     def add(self, other):
-        if isinstance(other, W_Int) or isinstance(other, W_Long):
-            return W_Str(str(self.value) + other.value)
-        elif isinstance(other, W_Str) or isinstance(other, W_Byte):
-            return W_Str(str(self.value) + other.value)
-        else:
-            raise Exception("Unexpected object: %s" % (other))
+        raise Exception(
+            "Unsupported operand type(s) for +: %s and %s" % (self.typ, other.typ)
+        )
 
     def sub(self, other):
         raise Exception(
@@ -149,6 +206,9 @@ class W_Byte(W_Root):
             raise Exception(
                 "Unsupported operand type(s) for *: %s and %s" % (self.typ, other.typ)
             )
+
+    def div(self, other):
+        raise Exception("Unsupported operaiton /")
 
 
 class W_Sequence(W_Root):
@@ -183,6 +243,11 @@ class W_List(W_Sequence):
 
 
 class W_Code(W_Root):
+    "for debugging. contains code object obtained from marshal.load"
+
+    def __init__(self, value):
+        self.value = value
+
     def __repr__(self):
         return repr(self.value)
 
