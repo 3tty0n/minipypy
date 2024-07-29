8b6b34bdff82731306d7439cccb4ba14289e625a
Yusuke Izawa <me@yizawa.com>
2024-07-29 21:09:30 +0900

    Add disassemble.py

---
+++ disassemble.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+import argparse
+import dis
+import marshal
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='compile')
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    f = open(args.filename, 'rb')
+    f.read(4)
+    f.read(4)
+    code = marshal.load(f)
+    dis.disassemble(code)
