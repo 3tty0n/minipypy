57818ff29219b554dd528aa005df1c56fe167d95
Yusuke Izawa <me@yizawa.com>
2024-09-17 15:01:12 +0900

    Move to dictobject.py

--- minipypy/interpret.py
+++ minipypy/interpret.py
@@ -151,51 +151,6 @@ class LoopBlock(FrameBlock):
             return r_uint(self.handlerposition)
 
 
-class Map(object):
-    def __init__(self):
-        self.indexes = {}
-        self.other_maps = {}
-
-    @jit.elidable
-    def getindex(self, name):
-        return self.indexes.get(name, -1)
-
-    @jit.elidable
-    def new_map_with_additional_name(self, name):
-        if name not in self.other_maps:
-            newmap = Map()
-            newmap.indexes.update(self.indexes)
-            newmap.indexes[name] = len(self.indexes)
-            self.other_maps[name] = newmap
-        return self.other_maps[name]
-
-
-EMPTY_MAP = Map()
-
-
-class W_Dict(W_RootObject):
-
-    def __init__(self):
-        self.map = EMPTY_MAP
-        self.storage = []
-
-    def __getitem__(self, w_key):
-        map = promote(self.map)
-        index = map.getindex(w_key)
-        if index != -1:
-            return self.storage[index]
-        return None
-
-    def __setitem__(self, w_key, w_val):
-        map = promote(self.map)
-        index = map.getindex(w_key)
-        if index != -1:
-            self.storage[index] = w_val
-            return
-        self.map = map.new_map_with_additional_name(w_key)
-        self.storage.append(w_val)
-
-
 class PyFrame(W_RootObject):
     _virtualizable_ = [
         "last_intr", "code",
---
+++ minipypy/objects/dictobject.py
@@ -0,0 +1,47 @@
+from rpython.rlib.jit import elidable, promote
+
+from minipypy.objects.baseobject import W_RootObject
+
+class Map(object):
+    def __init__(self):
+        self.indexes = {}
+        self.other_maps = {}
+
+    @elidable
+    def getindex(self, name):
+        return self.indexes.get(name, -1)
+
+    @elidable
+    def new_map_with_additional_name(self, name):
+        if name not in self.other_maps:
+            newmap = Map()
+            newmap.indexes.update(self.indexes)
+            newmap.indexes[name] = len(self.indexes)
+            self.other_maps[name] = newmap
+        return self.other_maps[name]
+
+
+EMPTY_MAP = Map()
+
+
+class W_Dict(W_RootObject):
+
+    def __init__(self):
+        self.map = EMPTY_MAP
+        self.storage = []
+
+    def __getitem__(self, w_key):
+        map = promote(self.map)
+        index = map.getindex(w_key)
+        if index != -1:
+            return self.storage[index]
+        return None
+
+    def __setitem__(self, w_key, w_val):
+        map = promote(self.map)
+        index = map.getindex(w_key)
+        if index != -1:
+            self.storage[index] = w_val
+            return
+        self.map = map.new_map_with_additional_name(w_key)
+        self.storage.append(w_val)
